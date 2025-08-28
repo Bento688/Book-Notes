bed4ddf58aad56a503356b44d8559b36dca7613e
Bento688 <benedict.halkin688@gmail.com>
2025-08-28 14:04:25 +0800

    hide details lol

--- index.js
+++ index.js
@@ -13,12 +13,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 //Connect DB
+const dbPw = process.env.DB_PASSWORD;
+const dbDb = process.env.DB;
+const dbUser = process.env.DB_USER;
+const dbPort = process.env.DB_PORT;
+const dbHost = process.env.DB_HOST;
+
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "books",
-  password: "Hironimusjnk4732",
-  port: 5432,
+  user: dbUser,
+  host: dbHost,
+  database: dbDb,
+  password: dbPw,
+  port: parseInt(dbPort),
 });
 db.connect();
 
