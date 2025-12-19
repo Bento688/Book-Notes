8eafe50219dfba61a72a92b76135289d2bedf9de
Bento688 <benedict.halkin688@gmail.com>
2025-12-19 14:19:17 +0800

    test for rebuilding

--- schema.sql
+++ schema.sql
@@ -45,3 +45,5 @@ CREATE TABLE "session" (
 );
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
 CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+--tt
